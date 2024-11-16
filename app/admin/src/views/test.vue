668edabefc8feba6b20143875acb3ccc889aeef5
mufeiyu <2382839439@qq.com>
2024-11-16 23:07:59 +0800

    feat: turbo测试

--- app/admin/src/views/test.vue
+++ app/admin/src/views/test.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { formatDate } from '@ayu-mu/utils'
+import { AyuButton } from '@ayu-mu/common'
 // import { onMounted } from 'vue'
 const date = new Date()
 console.log(formatDate(date, 'HH:mm'), '日期格式')
@@ -10,5 +11,8 @@ console.log(formatDate, 'formatDate')
 </script>
 
 <template>
-  <div>this is a test page</div>
+  <div>this is a 1111test page</div>
+  <div>
+    <ayu-button type="success">hello world</ayu-button>
+  </div>
 </template>
