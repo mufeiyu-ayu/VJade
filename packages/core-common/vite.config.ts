01d2dbae2ef6232ef06b413e37abd4d8dab51586
mufeiyu <2382839439@qq.com>
2025-02-03 11:37:37 +0800

    feat: 新增插件压缩打包

--- packages/core-common/vite.config.ts
+++ packages/core-common/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import { definePkgConfig } from '@ayu-mu/vite'
 import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer'
+
+// import terser from '@rollup/plugin-terser'
 export default defineConfig(async () => {
   return definePkgConfig({
     name: 'AyuBasicComponent',
@@ -10,12 +12,28 @@ export default defineConfig(async () => {
       plugins: [
         vue(),
         visualizer({
-          open: true, // 自动打开分析页面
+          open: false, // 自动打开分析页面
           filename: 'dist/stats.html', // 分析文件名
           gzipSize: true, // 显示 gzip 大小
           brotliSize: true, // 显示 brotli 大小
           template: 'treemap' // 视图类型: sunburst, treemap, network
         })
+        // terser({
+        //   compress: {
+        //     drop_console: true, // 移除 console
+        //     drop_debugger: true, // 移除 debugger
+        //     pure_funcs: ['console.log'] // 移除 console.log
+        //   },
+        //   format: {
+        //     comments: false, // 删除注释
+        //     beautify: false, // 不美化输出
+        //     indent_level: 0, // 缩进级别为 0
+        //     width: 0
+        //   },
+        //   mangle: {
+        //     properties: false // 不压缩属性名
+        //   }
+        // })
       ]
     }
   })
