b9d889fa11f208824494a9637dbbcb76b87718f2
mufeiyu <2382839439@qq.com>
2025-02-12 00:01:41 +0800

    feat: 优化server 模块

--- server/server/routes/[...].ts
+++ server/server/routes/[...].ts
@@ -4,7 +4,7 @@ export default defineEventHandler(() => {
 <h2>Mock service is starting</h2>
 <ul>
 <li><a href="/api/user">/api/user/info</a></li>
-<li><a href="/api/menu">/api/menu/all</a></li>
+<li><a href="/api/menu/menu">/api/menu/menu</a></li>
 <li><a href="/api/auth/codes">/api/auth/codes</a></li>
 <li><a href="/api/auth/login">/api/auth/login</a></li>
 <li><a href="/api/test">/api/test</a></li>
