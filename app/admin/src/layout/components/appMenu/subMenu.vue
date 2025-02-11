abb874efad25dcc72130ae39f548fef4fffd1520
mufeiyu <2382839439@qq.com>
2025-02-11 23:59:00 +0800

    feat: 搭建 layout 首页

---
+++ app/admin/src/layout/components/appMenu/subMenu.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { MenuItem } from '@ayu-mu/model'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const props = defineProps<{
+  menuList: MenuItem[]
+}>()
+
+const route = useRoute()
+const activeIndex = ref('')
+
+// 根据路由查找对应的菜单项
+const findMenuByRoute = (menus: MenuItem[], path: string): string => {
+  for (const menu of menus) {
+    if (menu.routePath === path) {
+      return menu.menuIndex
+    }
+    if (menu.children) {
+      const found = findMenuByRoute(menu.children, path)
+      if (found) return found
+    }
+  }
+  return ''
+}
+
+// 初始化选中状态
+onMounted(() => {
+  // 优先从 localStorage 获取
+  const savedIndex = localStorage.getItem('activeMenuIndex')
+  if (savedIndex) {
+    activeIndex.value = savedIndex
+  } else {
+    // 如果没有保存的状态，则根据当前路由设置
+    const currentPath = route.path.replace(/^\//, '') // 移除开头的斜杠
+    const menuIndex = findMenuByRoute(props.menuList, currentPath)
+    if (menuIndex) {
+      activeIndex.value = menuIndex
+      localStorage.setItem('activeMenuIndex', menuIndex)
+    }
+  }
+})
+
+// 处理菜单选中
+const handleSelect = (index: string) => {
+  activeIndex.value = index
+  localStorage.setItem('activeMenuIndex', index)
+}
+</script>
+
+<template>
+  <template v-for="item in menuList" :key="item.menuIndex">
+    <el-sub-menu v-if="item.children" :index="item.menuIndex" :class="{ 'is-active': activeIndex === item.menuIndex }">
+      <template #title>
+        <el-icon>
+          <component :is="item.icon" />
+        </el-icon>
+        <span>{{ item.menuTitle }}</span>
+      </template>
+      <subMenu :menuList="item.children" />
+    </el-sub-menu>
+    <el-menu-item v-else :index="item.menuIndex" @click="handleSelect(item.menuIndex)">
+      <el-icon>
+        <component :is="item.icon" />
+      </el-icon>
+      <span>{{ item.menuTitle }}</span>
+    </el-menu-item>
+  </template>
+</template>
+
+<style scoped lang="scss">
+:deep(.el-menu-item.is-active) {
+  background-color: var(--el-menu-hover-bg-color);
+}
+
+:deep(.el-sub-menu.is-active) {
+  > .el-sub-menu__title {
+    color: var(--el-menu-active-color);
+  }
+}
+</style>
---
+++ app/admin/src/layout/components/menu.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import menuList from './data'
+import subMenu from './appMenu/subMenu.vue'
+// const handleOpen = (key: string, keyPath: string[]) => {
+//   console.log(key, keyPath)
+// }
+// const handleClose = (key: string, keyPath: string[]) => {
+//   console.log(key, keyPath)
+// }
+</script>
+<template>
+  <div class="w-full h-full">
+    <el-menu default-active="2" class="el-menu-vertical-demo">
+      <subMenu :menuList="menuList" />
+    </el-menu>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
