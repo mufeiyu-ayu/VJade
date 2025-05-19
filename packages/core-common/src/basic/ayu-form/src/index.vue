5a1474a32e38bb1c5019fb0beb3b4c0ad3c873c6
ayu <2382839439@qq.com>
2025-05-19 23:14:33 +0800

    feat: 优化 git 提交

--- packages/core-common/src/basic/ayu-form/src/index.vue
+++ packages/core-common/src/basic/ayu-form/src/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { ElForm, ElFormItem, ElButton, ElRow, ElCol } from 'element-plus'
+import type { FormProps, NormalFormProps } from './types'
 
 import { AyuIcon } from '@ayu-mu/common'
-import type { FormProps, NormalFormProps } from './types'
+import { ElButton, ElCol, ElForm, ElFormItem, ElRow } from 'element-plus'
 import { useForm } from './hooks/ayu-form-hooks'
+
 const props = defineProps<FormProps>()
 const {
   formRef,
@@ -18,7 +19,7 @@ const {
   handleCollapseChange,
   collapseIsExpand,
   tabsOverflowY,
-  componentRender
+  componentRender,
 } = useForm()
 
 defineExpose({
@@ -26,54 +27,76 @@ defineExpose({
   formRef,
   resetForm,
   submitForm,
-  formData
+  formData,
 })
 </script>
 
 <template>
   <div class="w-full">
-    <el-form ref="formRef" label-position="right" label-width="auto" :rules="formRules" :model="formData">
-      <component :is="groupComponent" :type="tabType" v-model="allActiveName" @change="handleCollapseChange">
+    <ElForm
+      ref="formRef"
+      label-position="right"
+      label-width="auto"
+      :rules="formRules"
+      :model="formData"
+    >
+      <component
+        :is="groupComponent"
+        v-model="allActiveName"
+        :type="tabType"
+        @change="handleCollapseChange"
+      >
         <template v-for="group in formGroupConfig" :key="group.groupTitle">
-          <component :is="groupItemConfig" :label="group.groupTitle" :title="group.groupTitle" :name="group.groupTitle">
-            <div class="flex justify-start items-center p-4 mb-[10px]" v-if="groupComponent === 'div' && isGroup">
-              <div class="w-[5px] h-[20px] bg-[#409EFF]"></div>
-              <div class="text-[18px] font-bold ml-[15px]">{{ group.groupTitle }}</div>
+          <component
+            :is="groupItemConfig"
+            :label="group.groupTitle"
+            :title="group.groupTitle"
+            :name="group.groupTitle"
+          >
+            <div v-if="groupComponent === 'div' && isGroup" class="flex justify-start items-center p-4 mb-[10px]">
+              <div class="w-[5px] h-[20px] bg-[#409EFF]" />
+              <div class="text-[18px] font-bold ml-[15px]">
+                {{ group.groupTitle }}
+              </div>
             </div>
             <!-- todo collapse-item icon -->
-            <template #icon v-if="groupType === 'collapse'">
-              <div class="ml-1"></div>
+            <template v-if="groupType === 'collapse'" #icon>
+              <div class="ml-1" />
               <AyuIcon :icon="collapseIsExpand.includes(group.groupTitle) ? 'mdi:chevron-down' : 'mdi:chevron-up'" />
             </template>
             <div :class="tabsOverflowY">
-              <el-row>
-                <el-col
+              <ElRow>
+                <ElCol
                   v-for="field in group.fieldConfig"
                   :key="field.field"
                   :span="isGroup ? field.colSize : (props as NormalFormProps).colSize || field.colSize"
                 >
-                  <el-form-item :label="field.label" :prop="field.field">
+                  <ElFormItem :label="field.label" :prop="field.field">
                     <component
-                      v-model="formData[field.field]"
                       :is="componentRender(field.type)"
+                      v-model="formData[field.field]"
                       v-bind="{
-                        ...field.componentProps
+                        ...field.componentProps,
                       }"
-                    ></component>
-                  </el-form-item>
-                </el-col>
-              </el-row>
+                    />
+                  </ElFormItem>
+                </ElCol>
+              </ElRow>
             </div>
           </component>
         </template>
       </component>
       <slot>
         <div class="w-full mt-10 flex justify-center">
-          <el-button type="primary" @click="submitForm">提交</el-button>
-          <el-button @click="resetForm">重置11</el-button>
+          <ElButton type="primary" @click="submitForm">
+            提交
+          </ElButton>
+          <ElButton @click="resetForm">
+            重置11
+          </ElButton>
         </div>
       </slot>
-    </el-form>
+    </ElForm>
   </div>
 </template>
 
