7d3e2faf35942583fd7604b5948622337427dad4
ayu <2382839439@qq.com>
2025-07-02 00:16:00 +0800

    feat: 测试提交

--- packages/request/src/http/index.ts
+++ packages/request/src/http/index.ts
@@ -2,7 +2,6 @@ import type { CreateAxiosOptionsType } from '@ayu-mu/model'
 import { AyuAxios } from './Axios'
 
 // 全局请求默认配置
-
 const defaultOptions: CreateAxiosOptionsType = {
   timeout: 100000,
   requestOptions: {
