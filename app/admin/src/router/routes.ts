953e84ffadc66465104f8ba43d60d5458dfe1e58
ayu <2382839439@qq.com>
2025-07-08 23:02:57 +0800

    feat: 测试提交

--- app/admin/src/router/routes.ts
+++ app/admin/src/router/routes.ts
@@ -3,5 +3,5 @@ import { EXCEPTION_ROUTE, LOGIN_ROUTE, NOT_FOUND_ROUTE } from './modules/base'
 
 import { HOME_ROUTE } from './modules/home'
 
-// 路由信息
+// 基础路由
 export const routes: Readonly<RouteRecordRaw[]> = [HOME_ROUTE, LOGIN_ROUTE, EXCEPTION_ROUTE, NOT_FOUND_ROUTE]
