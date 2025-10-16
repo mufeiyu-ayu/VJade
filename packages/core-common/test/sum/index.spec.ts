16b21cb4c3b3c974c8a08a47eb87b46b3cd1fdec
ayu <2382839439@qq.com>
2025-10-17 01:15:51 +0800

    feat: 优化

--- packages/core-common/test/sum/index.spec.ts
+++ packages/core-common/test/sum/index.spec.ts
@@ -1,11 +1,14 @@
-import { sum } from './index.ts'
+import axios from 'axios'
 
-it('sum', () => {
-  expect(sum(1, 2)).toBe(3)
+// 注册 mock
+vi.mock('axios')
+
+it('mock axios.get', async () => {
+  vi.mocked(axios.get).mockResolvedValue({
+    data: { code: 200, message: 'success', data: { name: 'John' } },
+  })
+
+  const res = await axios.get('/user')
+  console.log(res, 'res')
+  expect(res.data.data.name).toBe('John')
 })
-
-const mockSum = vi.fn().mockImplementation(sum)
-mockSum(2, 3)
-console.log(mockSum.getMockName(), '--11')
-console.log(mockSum.getMockImplementation(), '--')
-console.log(mockSum.getMockImplementation(), '--')
